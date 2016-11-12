3a96f16b5002673e9344f809153fb19653b3d5a5
rockett1 <rockett1000@googlemail.com>
2016-11-12 11:53:04 +0000

    Week 5 tasks

---
+++ W5Q1.py
@@ -0,0 +1,20 @@
+L = [1,2,3,4,5,6,7,8,1,5,1,3,6,7,8,9,2,1]
+
+def subseq(L):
+    dic = {'Longest_Increasing_Sub-sequence':['1']}
+    tempList = []
+    i = 0
+    while i < (len(L)-1):   
+        if (L[i] < L[i+1]):
+            tempList.append(L[i])
+        else:
+            tempList.append(L[i])   #adds last number parsed into templist to complete sub string
+            if len(dic['Longest_Increasing_Sub-sequence']) < len(tempList): #compares if new substring is longer than one stored in dictionary
+                print(tempList)
+                dic['Longest_Increasing_Sub-sequence'] = tempList 
+            tempList = []
+        i += 1
+    print(dic)
+            
+
+subseq(L)
---
+++ W5Q2.py
@@ -0,0 +1,50 @@
+
+class Node(object):
+    def __init__(self, value):
+        self.value=value
+        self.next=None
+        self.prev=None
+
+class List(object):
+    def __init__(self):
+        self.head=None
+        self.tail=None
+    def insert(self,n,x):
+        #Not actually perfect: how do we prepend to an existing list?
+        if n!=None:
+            x.next=n.next
+            n.next=x
+            x.prev=n
+            if x.next!=None:
+                x.next.prev=x              
+        if self.head==None:
+            self.head=self.tail=x
+            x.prev=x.next=None
+        elif self.tail==n:
+            self.tail=x
+    def delete(self,nvalue):
+        node = self.head
+        prevNode = None
+        while node != None:
+            if node.value == nvalue:    #if node is one to delete
+                if prevNode != None:    #to avoid error in removing first node do check
+                    prevNode.next = node.next   
+                else:
+                    self.head = node.next
+            prevNode = node             #changes value of prevNode each iteration
+            node = node.next            #changes value of node each iteration
+            
+    def display(self):
+        values=[]
+        n=self.head
+        while n!=None:
+            values.append(str(n.value))
+            n=n.next
+        print ("List: ",",".join(values))
+          
+if __name__ == '__main__':
+    l=List()
+    l.insert(None, Node(4))
+    l.insert(l.head,Node(6))
+    l.insert(l.head,Node(8))
+    l.display()
